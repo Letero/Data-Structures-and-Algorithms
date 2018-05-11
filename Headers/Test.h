87fce678d16e789905b7f601115a61cd8088f471
Kurzynski Jaroslaw <jaroslaw.kurzynski@tieto.com>
2018-05-11 11:54:02 +0200

    first draft of custom tests added

---
+++ Headers/Test.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H
+#define TEST_H
+#include "Common.h"
+
+/**
+ *  param[in] return value of tested funtion
+ *  param[in] name of tested funtion
+ *  return void
+*/
+void TEST(eError_t retVal, char *fun_name);
+
+#endif // TEST_H
