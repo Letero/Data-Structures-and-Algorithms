0307c5de0cda917d04cf5f0fa3fc4f10dd195c10
Kurzynski Jaroslaw <jaroslaw.kurzynski@tieto.com>
2019-02-25 14:25:27 +0100

    Added copy constructor, clear(), reallocateMem()

--- STL_Vector/Vector.h
+++ STL_Vector/Vector.h
@@ -1,33 +1,51 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-const int RESERVE = 50; //we don't  want to resive vector every time we add new elem
+const int RESERVE = 50; //we don't want to resive vector every time we add new elem
 
 template <class T>
 class Vector
 {
 public:
-  /*member functions*/
-  Vector() : reserve(RESERVE)
+  /*constructors*/
+  Vector() : reserve(RESERVE) //default
   {
     arr = new T[reserve];
     _size = 0;
   }
-  ~Vector()
+  Vector(const Vector &cpy) : reserve(RESERVE) //copy constructor
+  {
+    this->arr = cpy.arr;
+    this->_size = cpy._size;
+  }
+  ~Vector() //deconstructor
   {
     delete[] arr;
   }
+
   void clear() //clear the contents of vector
   {
+    _size = 0;
+    delete[] arr;
+    T *nArr = new T[reserve];
+    arr = nArr;
   }
-  void assign() {}                //
-  void erase(unsigned int pos) {} //
+  void assign() {} //
+  void erase(unsigned int pos)
+  {
+    
+  }
   void push_back(T elem)
   {
     arr[_size] = elem;
     _size++;
+    if (_size % RESERVE == 0)
+    {
+      this->reallocateMem();
+    }
   }
   void pop_back() { --_size; }
+
   unsigned int size() { return _size; }
 
   T *begin() { return &arr[0]; }
@@ -38,7 +56,13 @@ private:
   T *arr;
   const unsigned int reserve;
   unsigned int _size;
-  void reallocateMem(); //probably using move operator?
+  void reallocateMem()
+  {
+    T *nArr = new T[_size + RESERVE];
+    memcpy(nArr, arr, (_size + RESERVE) * sizeof(T));
+    delete[] arr;
+    arr = nArr;
+  }
 };
 
 #endif //VECTOR_H
--- STL_Vector/main.cpp
+++ STL_Vector/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include "Vector.h"
 #include <vector>
+
 int main(int argc, char *argv[])
 {
-    Vector<std::string> vec;
-    vec.push_back("5");
-    vec.pop_back();
-    std::cout << vec.size() << std::endl;
-    std::cout << *vec.begin() << std::endl;
+    Vector<int> vec;
+    vec.push_back(5);
+    vec.push_back(10);
+    vec.push_back(11);
+    vec.push_back(23);
+
+    //Vector<int> vec2(vec);
+    for (auto a : vec)
+    {
+        std::cout << a << " ";
+    }
+    //std::cout << *vec.begin() << std::endl;
 
     std::vector<std::string> v;
     v.push_back("5");
-    v.pop_back();
-    std::cout << v.size() << std::endl;
-    std::cout << *v.begin() << std::endl;
+
+    //std::cout << v.size() << std::endl;
+    //std::cout << v.begin() << std::endl;
 
     return 0;
 }
