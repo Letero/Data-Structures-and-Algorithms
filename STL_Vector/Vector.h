7f55615a0bec5334a9a4bbca84d5283bd20f01ac
Kurzynski Jaroslaw <jaroslaw.kurzynski@tieto.com>
2019-02-25 17:19:05 +0100

    Random access operator added

--- STL_Vector/Vector.h
+++ STL_Vector/Vector.h
@@ -2,18 +2,19 @@
 #define VECTOR_H
 #include <iostream>
 
-const int RESERVE = 50; //we don't want to resive vector every time we add new elem
+const int RESERVE = 2; //we don't want to resive vector every time we add new elem
 
 template <class T>
 class Vector
 {
 public:
   /*constructors*/
-  Vector() : reserve(RESERVE) //default
+  //default
+  Vector() : _reserve(RESERVE), _memSize(RESERVE)
   {
     try
     {
-      arr = new T[reserve];
+      arr = new T[_reserve];
       _size = 0;
     }
     catch (std::bad_alloc &ba)
@@ -21,23 +22,37 @@ public:
       std::cerr << "bad_alloc caught: " << ba.what() << '\n';
     }
   }
-  Vector(const Vector &cpy) : reserve(RESERVE) //copy constructor
+  //copy constructor
+  Vector(const Vector &old) : _reserve(RESERVE)
   {
-    this->arr = cpy.arr;
-    this->_size = cpy._size;
+    try
+    {
+      arr = new T[old._memSize];
+      this->arr = old.arr;
+      this->_size = old._size;
+      this->_memSize = old._memSize;
+    }
+    catch (std::bad_alloc &ba)
+    {
+      std::cerr << "bad_alloc caught: " << ba.what() << '\n';
+    }
   }
-  ~Vector() //deconstructor
+  //deconstructor
+  ~Vector()
   {
     delete[] arr;
   }
 
+  /*non-constructor members*/
+
   void clear() //clear the contents of vector
   {
     _size = 0;
+    _memSize = _reserve;
     delete[] arr;
     try
     {
-      T *nArr = new T[reserve];
+      T *nArr = new T[_memSize];
       arr = nArr;
     }
     catch (std::bad_alloc &ba)
@@ -45,16 +60,20 @@ public:
       std::cerr << "bad_alloc caught: " << ba.what() << '\n';
     }
   }
-  /*non-constructor members*/
   void assign() {} //
   void erase(unsigned int pos)
   {
+    if (pos >= 0 && pos < _size)
+    {
+    }
+    else
+      throw "erase() : Position out of range\n";
   }
   void push_back(T elem)
   {
     arr[_size] = elem;
     _size++;
-    if (_size % RESERVE == 0)
+    if (_size % _reserve == 0)
     {
       this->reallocateMem();
     }
@@ -69,13 +88,14 @@ public:
 
   T at(long long int pos)
   {
-    if (pos > 0 && pos < _size)
+    if (pos >= 0 && pos < _size)
       return arr[pos];
     else
       throw "at() : Position out of range\n";
   }
 
   /*operators*/
+  //assign operator
   Vector &operator=(const Vector &old)
   {
     if (this->arr)
@@ -87,17 +107,24 @@ public:
     this->arr = old.arr;
     return *this;
   }
+  //random access operator
+  int &operator[](const int pos)
+  {
+    return arr[pos];
+  }
 
 private:
   T *arr;
-  const unsigned int reserve;
+  const unsigned int _reserve;
+  unsigned int _memSize;
   unsigned int _size;
   void reallocateMem()
   {
     try
     {
-      T *nArr = new T[_size + RESERVE];
-      memcpy(nArr, arr, (_size + RESERVE) * sizeof(T));
+      _memSize += _reserve;
+      T *nArr = new T[_memSize];
+      memcpy(nArr, arr, (_memSize) * sizeof(T));
       delete[] arr;
       arr = nArr;
     }
